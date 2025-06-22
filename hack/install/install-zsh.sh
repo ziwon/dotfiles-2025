44122ecec302db3c72af76c5b75e95b833cefba0
Aaron Yoon <yngpil.yoon@gmail.com>
2025-06-22 19:04:47 +0900

    feat: update zsh conf.

--- hack/install/install-zsh.sh
+++ hack/install/install-zsh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install and configure modern Zsh setup
 
+CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
 echo "🚀 Installing modern Zsh setup..."
 
 # Install Zsh as default shell
@@ -12,8 +14,18 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 mkdir -p "$(dirname $ZINIT_HOME)"
 git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 
+
+# Download Oh My Posh themes
+if [ ! -d "$HOME/.poshthemes" ]; then
+    echo "Downloading poshthemes..."
+    mkdir -p "$HOME/.poshthemes"
+    curl -sL https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -o "$HOME/themes.zip"
+    unzip -qo "$HOME/themes.zip" -d "$HOME/.poshthemes"
+    rm "$HOME/themes.zip" 
+fi 
+
 # Copy configurations
-cp config/shell/zsh/zshrc ~/.zshrc
-cp config/shell/zsh/p10k.zsh ~/.p10k.zsh
+ln -sf $CUR_DIR/../../config/shell/zsh/zshenv ~/.zshenv
+ln -sf $CUR_DIR/../../config/shell/zsh/zshrc ~/.zshrc
 
 echo "✅ Zsh setup complete! Please restart your terminal or run 'zsh'"
